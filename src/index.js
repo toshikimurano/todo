e958d2c28e025773607825425868511f24aac717
toshikimurano <109050665+toshikimurano@users.noreply.github.com>
2022-07-14 16:59:51 +0900

    7.14時点

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // liタグ生成
+  const li = document.createElement("li");
+  console.log(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
